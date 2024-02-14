540e3d2d21623e7f7ae5065f2d2957d79ab812ba
andrprg <andrprg@yandex.ru>
2024-02-14 11:14:35 +0300

    feat: Установлен angular material

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ApiPrefixInterceptor } from './app/data/interceptors/api-prefix.interceptor';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    {provide: HTTP_INTERCEPTORS, useClass: ApiPrefixInterceptor, multi: true},
-  ]
+    { provide: HTTP_INTERCEPTORS, useClass: ApiPrefixInterceptor, multi: true },
+    provideAnimations()
+]
 }).catch((e) =>
   console.error(e)
 );  
