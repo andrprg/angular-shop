ea8296b3b893c956037b8b32d59071bb186e1700
andrprg <andrprg@yandex.ru>
2024-02-25 18:52:33 +0300

    feat(server): Реализовано обновление токена

--- server/routers/login.route.ts
+++ server/routers/login.route.ts
@@ -7,12 +7,11 @@ const tokenList = new Map();
 export function loginUser(req: Request, res: Response) {
     const { login, password } = req.body;
     const user = authenticate(login, password);
-    console.log("User ...", user);
 
     if (user) {
         const { id, name, email } = user;
         const token = jwt.sign({ id, name, email }, CONFIG.secret, { expiresIn: CONFIG.tokenLife })
-        const refreshToken = jwt.sign(user, CONFIG.refreshTokenSecret, { expiresIn: CONFIG.refreshTokenLife })
+        const refreshToken = jwt.sign({ id, name, email }, CONFIG.refreshTokenSecret, { expiresIn: CONFIG.refreshTokenLife })
         const response = {
             "token": token,
             "refreshToken": refreshToken,
@@ -23,5 +22,21 @@ export function loginUser(req: Request, res: Response) {
     } else {
         res.status(403).json({ message: 'Неверный логин или пароль.' });
     }
+}
 
+export function token(req: Request, res: Response)  {
+    const postData = req.body
+    console.log('1:', postData);
+    console.log('2:', tokenList);
+    if((postData.refreshToken) && (postData.refreshToken === tokenList.get(postData.refreshToken).refreshToken)) {
+        const { id, name, email } = postData;
+        const token = jwt.sign({ id, name, email }, CONFIG.secret, { expiresIn: CONFIG.tokenLife})
+        const response = {
+            "token": token,
+        }
+        tokenList.set(postData.refreshToken, {'refreshToken': postData.refreshToken, token});
+        res.status(200).json(response);        
+    } else {
+        res.status(404).send('Invalid request')
+    }
 }
--- server/server.ts
+++ server/server.ts
@@ -1,12 +1,13 @@
 import * as express from 'express';
 import {Application} from "express";
-import { loginUser } from './routers/login.route';
+import { loginUser, token } from './routers/login.route';
 const bodyParser = require('body-parser');
 
 const app: Application = express();
 app.use(bodyParser.json());
 
 app.route('/login').post(loginUser);
+app.route('/token').post(token);
 
 
 const httpServer = app.listen(9000, () => {
