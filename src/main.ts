87a4e232036f5860a57b63f087514bca3f530228
andrprg <andrprg@yandex.ru>
2024-02-29 12:28:48 +0300

    feat(auth): Обновлен пакет

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,6 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
-import { ApiPrefixInterceptor } from './app/data/interceptors/api-prefix.interceptor';
-import { provideAnimations } from '@angular/platform-browser/animations';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: ApiPrefixInterceptor, multi: true },
-    provideAnimations(),
-    provideHttpClient()
-  ]
-}).catch((e) =>
-  console.error(e)
-);  
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
