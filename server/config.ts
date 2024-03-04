fcc5a899ebbf7932ee8fc08821cf38b26e1557af
andrprg <andrprg@yandex.ru>
2024-03-04 13:44:26 +0300

    feat(auth): Добавлены стили и цвета

--- server/config.ts
+++ server/config.ts
@@ -1,6 +1,6 @@
 export const CONFIG = {
     "secret": "some-secret-shit-goes-here",
     "refreshTokenSecret": "some-secret-refresh-token-shit",
-    "tokenLife": 900,
+    "tokenLife": 900, // 15 minute
     "refreshTokenLife": 86400
 }
