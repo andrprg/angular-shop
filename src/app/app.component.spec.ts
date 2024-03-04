563639abcc72bcb65096a1e7d2bba3103b4fa035
andrprg <andrprg@yandex.ru>
2024-03-04 14:02:11 +0300

    feat(auth): исправлены тесты

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,21 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
 
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [AppComponent, HttpClientTestingModule]
+    });
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+  
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
--- src/app/presentation/login/login.component.spec.ts
+++ src/app/presentation/login/login.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -9,7 +10,7 @@ describe('LoginComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [LoginComponent, BrowserAnimationsModule ]
+      imports: [LoginComponent, BrowserAnimationsModule, HttpClientTestingModule ]
     });
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
