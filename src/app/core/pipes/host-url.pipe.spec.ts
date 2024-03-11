82ee0a6a16910b4436bcd08fb1d3ec4f9c7fac12
andrprg <andrprg@yandex.ru>
2024-03-11 16:39:22 +0300

    feat(pipe): Добавлен pipe

---
+++ src/app/core/pipes/host-url.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { HostUrlPipe } from './host-url.pipe';
+
+describe('HostUrlPipe', () => {
+  it('create an instance', () => {
+    const pipe = new HostUrlPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ src/app/core/pipes/host-url.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { HOST_URL } from 'src/environments/environment';
+
+@Pipe({
+  name: 'hostUrl',
+  standalone: true
+})
+export class HostUrlPipe implements PipeTransform {
+
+  transform(imageUrl: string): string {
+    return HOST_URL + '/' + imageUrl;
+  }
+
+}
