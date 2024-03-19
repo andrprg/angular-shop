27f84db32975bfb7f54b9c5e91288a1b740f2943
andrprg <andrprg@yandex.ru>
2024-03-19 16:57:02 +0300

    feat(product): Реализован компонент product-card

--- src/app/presentation/home/home.component.ts
+++ src/app/presentation/home/home.component.ts
@@ -1,30 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SpinnerService } from 'src/app/ui/spinner/spinner.service';
 import { ProductsService } from 'src/app/repository/products.service';
 import { Observable } from 'rxjs';
 import { Product } from 'src/app/domain/product';
-import { MatCardModule } from '@angular/material/card';
-import { HostUrlPipe } from 'src/app/core/pipes/host-url.pipe';
-import {MatDividerModule} from '@angular/material/divider';
-import { MatIconModule } from '@angular/material/icon';
-import { RatingComponent } from 'src/app/ui/rating/rating.component';
-import { RubPipe } from 'src/app/core/pipes/rub.pipe';
+import { ProductCardComponent } from 'src/app/ui/product-card/product-card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     CommonModule,
-    MatCardModule,
-    HostUrlPipe,
-    MatDividerModule,
-    MatIconModule,
-    RatingComponent,
-    RubPipe
+    ProductCardComponent,
   ],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent implements OnInit {
 
@@ -39,6 +30,9 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     const products = this.productsService.getProducts();
     this.products$ = this.spinnerService.showLoaderUntilCompleted(products);
+  }
 
+  trackByFn(index: number, product: Product): string {
+    return product.id;
   }
 }
---
+++ src/app/ui/product-card/product-card.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Product } from 'src/app/domain/product';
+import { ChangeDetectionStrategy } from '@angular/compiler';
+import { HostUrlPipe } from 'src/app/core/pipes/host-url.pipe';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { RatingComponent } from '../rating/rating.component';
+import { RubPipe } from 'src/app/core/pipes/rub.pipe';
+
+@Component({
+  selector: 'app-product-card',
+  standalone: true,
+  imports: [
+    CommonModule,
+    HostUrlPipe,
+    MatCardModule,
+    MatDividerModule,
+    MatIconModule,
+    RatingComponent,
+    RubPipe,
+  ],
+  templateUrl: './product-card.component.html',
+  styleUrls: ['./product-card.component.scss'],
+})
+
+export class ProductCardComponent {
+  @Input() product!: Product
+}
