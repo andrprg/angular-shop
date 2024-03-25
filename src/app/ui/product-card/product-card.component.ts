31200c823a264172ce106bc7241543b4df1c90b5
andrprg <andrprg@yandex.ru>
2024-03-25 16:42:27 +0300

    feat(product): Реализована навигация

--- src/app/ui/product-card/product-card.component.ts
+++ src/app/ui/product-card/product-card.component.ts
@@ -1,13 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Product } from 'src/app/domain/product';
-import { ChangeDetectionStrategy } from '@angular/compiler';
 import { HostUrlPipe } from 'src/app/core/pipes/host-url.pipe';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { RatingComponent } from '../rating/rating.component';
 import { RubPipe } from 'src/app/core/pipes/rub.pipe';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-card',
@@ -20,11 +20,12 @@ import { RubPipe } from 'src/app/core/pipes/rub.pipe';
     MatIconModule,
     RatingComponent,
     RubPipe,
+    RouterLink
   ],
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.scss'],
 })
 
 export class ProductCardComponent {
-  @Input() product!: Product
+  @Input() product!: Product;
 }
