dc22470e4d98f74078c262f3e86c56ba5a9b8aee
andrprg <andrprg@yandex.ru>
2024-03-28 17:15:29 +0300

    fix(cart):  Исправлены тесты

--- src/app/presentation/product-detail/product-detail.component.spec.ts
+++ src/app/presentation/product-detail/product-detail.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductDetailComponent } from './product-detail.component';
+import { provideRouter } from '@angular/router';
+import { HomeComponent } from '../home/home.component';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('ProductDetailComponent', () => {
   let component: ProductDetailComponent;
@@ -8,7 +12,14 @@ describe('ProductDetailComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [ProductDetailComponent]
+      imports: [ProductDetailComponent],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        provideRouter([
+          { path: '', component: HomeComponent }
+        ]),
+      ]
     });
     fixture = TestBed.createComponent(ProductDetailComponent);
     component = fixture.componentInstance;
--- src/app/ui/product-card/product-card.component.spec.ts
+++ src/app/ui/product-card/product-card.component.spec.ts
@@ -1,14 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductCardComponent } from './product-card.component';
+import { provideRouter } from '@angular/router';
+import { ProductDetailComponent } from 'src/app/presentation/product-detail/product-detail.component';
 
 describe('ProductCardComponent', () => {
   let component: ProductCardComponent;
   let fixture: ComponentFixture<ProductCardComponent>;
 
+  const product = {
+    id: '1',
+    imageUrl: 'assets/products/bruschetta-plate.jpg',
+    title: 'Bruschetta plate',
+    description: 'Lorem ipsum',
+    price: 15,
+    availableQuantity: 5,
+    avgRating: 4.5,
+    numRatings: 5,
+  };
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [ProductCardComponent]
+      imports: [ProductCardComponent],
+      providers: [
+        provideRouter([
+          { path: 'product/:id', component: ProductDetailComponent }
+        ]),
+      ]
     });
     fixture = TestBed.createComponent(ProductCardComponent);
     component = fixture.componentInstance;
@@ -16,6 +34,8 @@ describe('ProductCardComponent', () => {
   });
 
   it('should create', () => {
+    component.product = product;
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
 });
