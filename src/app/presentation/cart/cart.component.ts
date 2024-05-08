fc2bc53414e92d8cb93728ba9af712292f878ef9
andrprg <andrprg@yandex.ru>
2024-05-08 16:35:36 +0300

    feat(cart): Реализован компонент корзины

--- src/app/presentation/cart/cart.component.ts
+++ src/app/presentation/cart/cart.component.ts
@@ -3,6 +3,15 @@ import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { ProductsService } from 'src/app/repository/products.service';
 import { CartService } from 'src/app/application/cart.service';
+import { Observable } from 'rxjs';
+import { Item } from 'src/app/domain/items';
+import { SpinnerService } from 'src/app/ui/spinner/spinner.service';
+import { CartItemComponent } from './cart-item/cart-item.component';
+import { HostUrlPipe } from 'src/app/core/pipes/host-url.pipe';
+import { ButtonDeleteComponent } from './button-delete/button-delete.component';
+import { CartTotalComponent } from './cart-total/cart-total.component';
+import { LayoutService } from 'src/app/repository/layout.service';
+import { Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-cart',
@@ -10,6 +19,8 @@ import { CartService } from 'src/app/application/cart.service';
   imports: [
     CommonModule,
     MatCardModule,
+    CartItemComponent,
+    CartTotalComponent
   ],
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss'],
@@ -17,16 +28,21 @@ import { CartService } from 'src/app/application/cart.service';
 })
 export class CartComponent implements OnInit {
 
+  /**
+   * Корзина
+   */
+  items$!: Observable<Item[]>;
+  layoutType$!: Observable<string>;
+  readonly breakpoints = Breakpoints;
+
   constructor(
-    public productService: ProductsService,
-    public cartService: CartService
-  ) {}
+    public cartService: CartService,
+    private layoutService: LayoutService
+  ) {
+    this.layoutType$ = this.layoutService.layoutType$;
+  }
   
   ngOnInit(): void {
-    this.cartService.items$.subscribe(data => {
-      console.log('remote:', data);
-    });
+    this.items$ =  this.cartService.items$;       
+   }
   }
-
-  
-}
